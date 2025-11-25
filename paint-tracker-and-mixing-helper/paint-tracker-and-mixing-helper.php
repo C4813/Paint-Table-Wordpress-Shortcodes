32396c3b1305d3d6bf69386a736ac14271e58404
Cable <c4813eve@gmail.com>
2025-11-25 10:01:02 +0000

    Update paint-tracker-and-mixing-helper.php

--- paint-tracker-and-mixing-helper/paint-tracker-and-mixing-helper.php
+++ paint-tracker-and-mixing-helper/paint-tracker-and-mixing-helper.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Paint Tracker and Mixing Helper
  * Description: Shortcodes to display your miniature paint collection, as well as a mixing and shading helper for specific colours.
- * Version: 0.6.3
+ * Version: 0.7.0
  * Author: C4813
  * Text Domain: pct
  */
@@ -26,7 +26,7 @@ if ( ! class_exists( 'PCT_Paint_Table_Plugin' ) ) {
         const META_LINK     = '_pct_link'; // legacy single link
 
         // Plugin version (used for asset cache-busting)
-        const VERSION = '0.6.3';
+        const VERSION = '0.7.0';
 
         public function __construct() {
             add_action( 'init',                    [ $this, 'register_types' ] );
