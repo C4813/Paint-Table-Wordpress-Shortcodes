9ef2bacbc0b3364b5e6b1b55871ffb3686fdbcce
Cable <c4813eve@gmail.com>
2025-12-01 15:30:40 +0000

    Update uninstall.php

--- paint-tracker-and-mixing-helper/uninstall.php
+++ paint-tracker-and-mixing-helper/uninstall.php
@@ -7,7 +7,9 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-// Delete plugin options.
+/**
+ * Delete plugin options.
+ */
 delete_option( 'pct_mixing_page_url' );
 delete_option( 'pct_table_display_mode' );
 delete_option( 'pct_shade_hue_mode' );
